9cc08a2eafbea2a2489358a526cae35cac1e1844
Gustavo Guimarães <gu.guimaraesrdc9@gmail.com>
2024-03-17 23:21:56 -0300

    Aula processos completa

--- processos/view/Principal.java
+++ processos/view/Principal.java
@@ -8,5 +8,31 @@ public class Principal {
 		ProcessosController procCon = new ProcessosController();
 		String info = procCon.os();
 		System.out.println(info);
+
+
+		// String process = "C:\\Windows\\write.exe";
+		// procCon.callProcess(process);
+		
+		// String process = "C:\\Windows\\regedit.exe";
+		// procCon.callProcess(process);
+
+		// String process = "TASKLIST /FO TABLE";
+		// procCon.readProcess(process);
+
+		// String process = "PING -t10 www.google.com.br";
+		// procCon.readProcess(process);
+
+		// String process = "TRACERT www.uol.com.br";
+		// procCon.readProcess(process);
+
+		// String process = "ipconfig";
+		// procCon.readProcess(process);
+		
+		// String param = "cmd.exe";
+		// procCon.killProcess(param);
+		
+		// String param = "4208";
+		// procCon.killProcess(param);
+
 	}
 }
