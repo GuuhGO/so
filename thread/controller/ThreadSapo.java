87008dc3fa912e16820c9fc4ec23ecda69f31bc6
Gustavo Guimarães <gu.guimaraesrdc9@gmail.com>
2024-03-24 18:17:36 -0300

    Corrida de Sapos - Finalizada

--- thread/controller/ThreadSapo.java
+++ thread/controller/ThreadSapo.java
@@ -1,19 +1,40 @@
-/*4) Fazer uma aplicação de uma corrida de sapos, com 5 Threads, cada Thread 
-controlando 1 sapo. Deve haver um tamanho máximo para cada pulo do sapo (em 
-metros) e a distância máxima para que os sapos percorram. A cada salto, um sapo pode 
-dar um salto de 0 até o tamanho máximo do salto (valor aleatório). Após dar um salto, a 
+/*4) Fazer uma aplicaï¿½ï¿½o de uma corrida de sapos, com 5 Threads, cada Thread 
+controlando 1 sapo. Deve haver um tamanho mï¿½ximo para cada pulo do sapo (em 
+metros) e a distï¿½ncia mï¿½xima para que os sapos percorram. A cada salto, um sapo pode 
+dar um salto de 0 atï¿½ o tamanho mï¿½ximo do salto (valor aleatï¿½rio). Apï¿½s dar um salto, a 
 Thread, para cada sapo, deve mostrar no console, qual foi o tamanho do salto e quanto 
-o sapo percorreu. Assim que o sapo percorrer a distância máxima, a Thread deve 
-apresentar que o sapo chegou e qual sua colocação.
-Dica: O exercício deve ser resolvido todo em console, ou seja, como se estivesse sendo 
-narrado. Atenção para a forma de definir a ordem de chegada.*/
+o sapo percorreu. Assim que o sapo percorrer a distï¿½ncia mï¿½xima, a Thread deve 
+apresentar que o sapo chegou e qual sua colocaï¿½ï¿½o.
+Dica: O exercï¿½cio deve ser resolvido todo em console, ou seja, como se estivesse sendo 
+narrado. Atenï¿½ï¿½o para a forma de definir a ordem de chegada.*/
 package thread.controller;
 
-public class ThreadSapo extends Thread{
-	
-	public ThreadSapo() {
-		super();
+public class ThreadSapo extends Thread {
+	private String nome;
+	private int maxSalto = 5;
+	private int distTotal;
+	private int distPercorrida = 0;
+	private static int colocacao = 0;
+
+	public ThreadSapo(int distTotal, String nome) {
+		this.distTotal = distTotal;
+		this.nome = nome;
 	}
-	
-	
+
+	@Override
+	public void run() {
+		while (distPercorrida < distTotal) {
+			try {
+				int salto = (int) (Math.random() * maxSalto) + 1;
+				distPercorrida += salto;
+				System.out.printf("Sapo %s saltou %d - Percorreu %d\n", this.nome, salto, distPercorrida);
+				sleep(700);
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+		colocacao++;
+		System.out.printf("Sapo %s chegou em %dÂº lugar\n", this.nome, colocacao);
+	}
+
 }
--- thread/view/E04_Thread_View.java
+++ thread/view/E04_Thread_View.java
@@ -1,29 +1,22 @@
 /*3) Criar em java um projeto com uma Thread chamada ThreadVetor, que receba um 
-valor numérico e vetor como parâmetros. Caso o valor numérico seja par, a thread deve 
+valor numï¿½rico e vetor como parï¿½metros. Caso o valor numï¿½rico seja par, a thread deve 
 percorrer o vetor utilizando uma estrutura for (int i = 0 ; i < vet.length; i++) e contar o 
-tempo para percorrer o vetor. Caso o valor numérico seja ímpar, a thread deve percorrer 
+tempo para percorrer o vetor. Caso o valor numï¿½rico seja ï¿½mpar, a thread deve percorrer 
 o vetor utilizando uma estrutura foreach e contar o tempo para percorrer o vetor. No 
 final, ambas as possibilidades devem apresentar o tempo em segundos.
-A operação main deve gerar um vetor de 1000 posições com valores aleatórios de 1 a 
-100. Deve iniciar 2 ThreadVetor e para uma passar o número 1 e o vetor e para a outra, 
-passar o número 2 e o mesmo vetor*/
+A operaï¿½ï¿½o main deve gerar um vetor de 1000 posiï¿½ï¿½es com valores aleatï¿½rios de 1 a 
+100. Deve iniciar 2 ThreadVetor e para uma passar o nï¿½mero 1 e o vetor e para a outra, 
+passar o nï¿½mero 2 e o mesmo vetor*/
 package thread.view;
 
-import thread.controller.ThreadVetor;
+import thread.controller.ThreadSapo;
 
 public class E04_Thread_View {
-
 	public static void main(String[] args) {
-		int length = 1000;
-		int vet[] = new int[length];
-		for (int i : vet) {
-			i = (int) (Math.random() * 1001);
+		String nomes[] = { "A", "B", "C", "D", "E" };
+		for (int i = 0; i < 5; i++) {
+			Thread t = new ThreadSapo(20, nomes[i]);
+			t.start();
 		}
-		
-		Thread t1 = new ThreadVetor(2, vet);
-		Thread t2 = new ThreadVetor(1, vet);
-		t1.start();
-		t2.start();
 	}
-
 }
