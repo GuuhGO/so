29183b63684ca4983e6837d245940acc45c8d27f
Gustavo Guimarães <gu.guimaraesrdc9@gmail.com>
2024-03-29 19:45:03 -0300

    4 Cavaleiros - Finalizado

--- semaforo/controller/CavaleirosController.java
+++ semaforo/controller/CavaleirosController.java
@@ -8,6 +8,8 @@ public class CavaleirosController extends Thread {
 	private int distPercorrida;
 	private static int tocha = 1;
 	private static int pedra = 1;
+	private final static int saida = 3; 
+	private static int doors[] = { 1, 2, 3, 4 };
 	private int velocidade;
 	private int min = 2;
 	private int max = 3;
@@ -21,15 +23,18 @@ public class CavaleirosController extends Thread {
 
 	@Override
 	public void run() {
+		int contador = 0;
 		while (distPercorrida < distTotal) {
 			velocidade = (int) ((Math.random() * max) + min);
 			distPercorrida += velocidade;
+			contador += 1;
 			try {
 				sleep(50);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			System.out.printf("Cavaleiro #%02d andou %dm\n", num, distPercorrida);
+			if (contador % 50 == 0)
+				System.out.printf("Cavaleiro #%02d andou %dm\n", num, distPercorrida);
 
 			if (distPercorrida >= 500 && tocha == 1) {
 				try {
@@ -54,7 +59,24 @@ public class CavaleirosController extends Thread {
 			}
 		}
 		System.out.printf("Cavaleiro #%02d CHEGOU\n", num);
-		
+
+		int porta = 0;
+		try {
+			semaforo.acquire();
+			porta = randomDoor();
+			System.out.printf("Cavaleiro #%02d abriu a porta %d\n", num, porta);
+
+			if (porta != saida) {
+				System.out.printf("Cavaleiro #%02d foi DEVORADO\n", num);
+			} else {
+				System.out.printf("Cavaleiro #%02d SAIU\n", num);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
+
 	}
 
 	private void pickUpStone() {
@@ -69,5 +91,19 @@ public class CavaleirosController extends Thread {
 		max += 2;
 		min += 2;
 		System.out.printf("Cavaleiro #%02d PEGOU A TOCHA\n", num);
+	}
+
+	private static int randomDoor() {
+		int pos;
+		int valor = 0;
+		do {
+			pos = (int) (Math.random() * 4);
+			valor = doors[pos];
+			if (valor != -1) {
+				doors[pos] = -1;
+				break;
+			}
+		} while (doors[pos] == -1);
+		return valor;
 	}
 }
--- semaforo/view/CavaleirosView.java
+++ semaforo/view/CavaleirosView.java
@@ -8,7 +8,7 @@ public class CavaleirosView {
 
 	public static void main(String[] args) {
 		Semaphore semaphore = new Semaphore(1);
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 4; i++) {
 			CavaleirosController cavaleirosController = new CavaleirosController(i, semaphore);
 			cavaleirosController.start();
 		}
